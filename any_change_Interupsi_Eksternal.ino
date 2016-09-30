a055f4de990832c19b4e8c955f34b29c78d3d87c
Zoni Syahrianto <zoni_syahrianto@yahoo.com>
2016-09-30 22:53:18 +0700

    Add files via upload

---
+++ any_change_Interupsi_Eksternal.ino
@@ -0,0 +1,29 @@
+#include<LiquidCrystal.h>'
+LiquidCrystal lcd (A0,A1,A2,A3,A4,A5);
+unsigned int countLoop = 0;
+void setup(){
+  lcd.begin(16,2);
+  lcd.clear();
+  attachInterrupt(0, rutinInterupsi0,CHANGE);
+  attachInterrupt(1, rutinInterupsi1,CHANGE);
+}
+
+void loop(){
+  lcd.setCursor(0,0); lcd.print("Main Program");
+  lcd.setCursor(0,1); lcd.print("Count Loop"); lcd.print(countLoop);
+  countLoop++;
+  delay(1000);
+}
+
+void rutinInterupsi0(){
+  lcd.setCursor(0,0); lcd.print("Int0 Program");
+  lcd.setCursor(0,1); lcd.print("Interrupt INT0");
+  delay(2000);
+}
+
+void rutinInterupsi1(){
+  lcd.setCursor(0,0); lcd.print("Int1 Program");
+  lcd.setCursor(0,1); lcd.print("Interrupt INT1");
+  delay(2000);
+}
+
